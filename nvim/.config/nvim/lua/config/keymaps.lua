4ba2805d5b251a2a73770617849364f500ad0a2c
Miha77777ua <lv11miha@gmail.com>
2025-08-04 18:28:49 +0300

    ok

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.keymap.set;
+local map = vim.keymap.set
 
 map("n", "<leader>lt", "<cmd>terminal live-server<cr>", { desc = "Open LiveServer" })
-map("n", "<leader>o", "<cmd>terminal sass scss/main.scss:css/main.min.css --style compressed<cr>",
-  { desc = "Compile SCSS to CSS" })
+map(
+  "n",
+  "<leader>o",
+  "<cmd>terminal sass scss/main.scss:css/main.min.css --style compressed<cr>",
+  { desc = "Compile SCSS to CSS" }
+)
+map("n", "<leader>r", '<cmd>%s/class[^=]*="\\([^"]*\\)"/className={style.\\1}<cr>', { desc = "Regex for module files" })
